7519c1641b7e45f9ef7d766ef6e57ab7877d6b69
fparrale <fparrale@ucm.es>
2026-06-10 07:34:01 -0500

    cambio en env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 // src/environments/environment.prod.ts
 export const environment = {
-  baseUrl: 'http://localhost:4200',
   production: true,
-  apiUrl: 'https://TU_HOSTING.com/api',
+  apiUrl: 'https://franklinparrales.es/SGLumbalgia/api'
 };
