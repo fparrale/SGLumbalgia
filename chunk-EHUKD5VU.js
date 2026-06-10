a830142c08b7e1843adc6879956465d935d23e32
fparrale <fparrale@ucm.es>
2026-06-10 07:04:35 -0500

    Auto-generated commit

--- chunk-EHUKD5VU.js
+++
@@ -1,1 +0,0 @@
-var r={production:!0,apiUrl:"https://franklinparrales.es/SGLumbalgia/api"};export{r as a};
---
+++ chunk-FZ42WZC5.js
@@ -0,0 +1,1 @@
+var r={production:!0,apiUrl:"https://franklinparrales.es/SGLumbalgia"};export{r as a};
