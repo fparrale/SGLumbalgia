01a27454167a8818e07f38bc6cb4393088e94921
fparrale <fparrale@ucm.es>
2026-06-10 07:04:08 -0500

    correccion en env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   //baseUrl: 'http://localhost:4200',
   production: true  , //false para desarrollo, true para producción
   //apiUrl: 'http://localhost/backend/api',
-  apiUrl: 'https://franklinparrales.es/SGLumbalgia/api',
+  apiUrl: 'https://franklinparrales.es/SGLumbalgia',
 };
