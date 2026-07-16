8ab045e6fd41ecffa4c5ef68221fadd93dc3edbc
fparrale <fparrale@ucm.es>
2026-07-16 11:31:33 -0500

    correccion enviorenment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@
 export const environment = {
   //baseUrl: 'http://localhost:4200',
   production: true  , //false para desarrollo, true para producción
-  //apiUrl: 'http://localhost/backend/api',
+  //apiUrl: 'http://localhost/backend/api', esto es para ejecutar en local
   apiUrl: 'https://franklinparrales.es/SGLumbalgia/api',
 };
